596777de3199390d135c6b663ac7036bc6ed3a02
eric.miers.16 <eric.miers.15@cnu.edu>
2018-10-08 22:48:54 -0400

    Established sample application with proper directory structure

---
+++ apps/sampleapp/src/sampleapp.py
@@ -0,0 +1,12 @@
+# CNU LINUX USERS GROUP
+# ---------------------
+# app_name: sampleapp.py
+# author: Eric Miers
+# version/date: 10/8/2018
+
+# description: Basic src file
+# ---------------------
+
+#INSERT-CODE-BELOW
+#-----------------
+
---
+++ apps/sampleapp/tests/sampleappstest.py
@@ -0,0 +1,12 @@
+# CNU LINUX USERS GROUP
+# ---------------------
+# app_name: sampleapptest.py
+# author: Eric Miers
+# version/date: 10/8/2018
+
+# description: Tester file for sample app
+# ---------------------
+
+#INSERT-TESTS-BELOW
+#------------------
+
